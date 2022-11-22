06ffcc20af099780db3dbf46c6ce4e34e2da42b6
Marlon Harrison <marlon@ionic.io>
2022-11-22 17:00:34 -0600

    adding tab2,tab3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'demo-multipart',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+    CapacitorCookies: {
+      enabled: true,
+    }
+  },
   webDir: 'www',
   bundledWebRuntime: false
 };
